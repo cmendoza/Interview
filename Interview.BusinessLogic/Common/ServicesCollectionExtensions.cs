7f1d271924fc2b01922632cf95a760f18f4133d9
Carlos Mendoza <cmendozag@outlook.com>
2020-11-21 18:17:59 -0600

    Add ServicesCollectionExtensions.
    - Adds required services for BusinessLogic ssembly

--- Interview.BusinessLogic/Common/ServicesCollectionExtensions.cs
+++ Interview.BusinessLogic/Common/ServicesCollectionExtensions.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Interview.BusinessLogic.Customers.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Interview.BusinessLogic.Common
 {
-    public  class ServicesCollectionExtensions
+    public static class ServicesCollectionExtensions
     {
+        public static IServiceCollection AddBusinessLogicDependencies(this IServiceCollection services)
+        {
+            services
+                .AddScoped<OrdersContext>()
+                .AddSingleton<CustomerRepository>();
+
+            return services;
+        }
     }
 }
