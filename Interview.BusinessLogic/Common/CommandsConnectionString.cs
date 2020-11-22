738b996ceb1a3f809b438e6e17083803b86dd015
Carlos Mendoza <cmendozag@outlook.com>
2020-11-21 18:26:08 -0600

    Add CommandsConnectionString

---
+++ Interview.BusinessLogic/Common/CommandsConnectionString.cs
@@ -0,0 +1,9 @@
+﻿namespace Interview.BusinessLogic.Common
+{
+    public sealed class CommandsConnectionString
+    {
+        public CommandsConnectionString(string value) => Value = value;
+
+        public string Value { get; }
+    }
+}
