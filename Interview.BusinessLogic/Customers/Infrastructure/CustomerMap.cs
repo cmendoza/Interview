b06e1a6d358407295117921698bca0d8433d3c19
Carlos Mendoza <cmendozag@outlook.com>
2020-11-21 20:39:33 -0600

    Add CustomerMap

--- Interview.BusinessLogic/Customers/Infrastructure/CustomerMap.cs
+++ Interview.BusinessLogic/Customers/Infrastructure/CustomerMap.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Interview.BusinessLogic.Customers.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Interview.BusinessLogic.Customers.Infrastructure
 {
-    class CustomerMap
+    internal sealed class CustomerMap : IEntityTypeConfiguration<Customer>
     {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("Customers");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.LastName).HasMaxLength(50).IsRequired();
+
+            builder
+                .HasMany(x => x.Orders)
+                .WithOne(x => x.Customer)
+                .HasForeignKey(x => x.CustomerId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+        public static CustomerMap GetInstance() => new CustomerMap();
     }
 }
