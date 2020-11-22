264a6bb8f7168f7a384dc031d43f1cb2396720e0
Carlos Mendoza <cmendozag@outlook.com>
2020-11-22 10:06:05 -0600

    Improvements in customer class

--- Interview.BusinessLogic/Customers/Domain/Customer.cs
+++ Interview.BusinessLogic/Customers/Domain/Customer.cs
@@ -1,26 +1,44 @@
-﻿using Interview.BusinessLogic.Common;
+﻿using CSharpFunctionalExtensions;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Interview.BusinessLogic.Customers.Domain
 {
-    public class Customer : Entity
+    public class Customer : Common.Entity
     {
         protected Customer()
         {
-
+            Orders = new HashSet<Order>();
         }
 
-        public Customer(string firstName, string lastName)
+        protected Customer(string firstName, string lastName) : this()
         {
             FirstName = firstName;
             LastName = lastName;
         }
 
-        public string FirstName { get; set; }
-        public string LastName  { get; set; }
+        public virtual string FirstName { get; protected set; }
+        public virtual string LastName  { get; protected set; }
 
         public virtual ICollection<Order> Orders { get; set; }
+
+        public static Result<Customer> Create(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName)) return Result.Failure<Customer>("First name is required.");
+
+            if (string.IsNullOrWhiteSpace(lastName)) return Result.Failure<Customer>("Last name is required.");
+
+            return Result.Success(new Customer(firstName, lastName));
+        }
+
+        public void UpdateProfileInfo(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName)) throw new ArgumentNullException("First name is required.");
+
+            if (string.IsNullOrWhiteSpace(lastName)) throw new ArgumentNullException("Last name is required.");
+
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 }
