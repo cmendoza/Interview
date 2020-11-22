333b5962d02a9a8b81aa9bde9d2780bce92337d6
Carlos Mendoza <cmendozag@outlook.com>
2020-11-21 20:46:08 -0600

    Clean up OrdersContext, added CustomerMap to OnModelCreating method

--- Interview.BusinessLogic/Common/OrdersContext.cs
+++ Interview.BusinessLogic/Common/OrdersContext.cs
@@ -1,5 +1,4 @@
-﻿using Interview.BusinessLogic.Customers.Domain;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Interview.BusinessLogic.Common
 {
@@ -12,11 +11,6 @@ namespace Interview.BusinessLogic.Common
             _connectionString = connectionString;
         }
 
-        // public virtual DbSet<Customer> Customers { get; set; }
-        public virtual DbSet<Order> Orders { get; set; }
-        //public virtual DbSet<OrderItem> OrderItems { get; set; }
-        //public virtual DbSet<Product> Products { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(_connectionString);
@@ -24,63 +18,8 @@ namespace Interview.BusinessLogic.Common
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Customer>(entity =>
-            {
-                entity.Property(e => e.FirstName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.LastName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
-            modelBuilder.Entity<Order>(entity =>
-            {
-                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
-
-                entity.Property(e => e.Total).HasColumnType("decimal(10, 2)");
-
-                entity.HasOne(d => d.Customer)
-                    .WithMany(p => p.Orders)
-                    .HasForeignKey(d => d.CustomerId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Orders_Customers");
-
-                entity.HasMany(x => x.OrderItems)
-                    .WithOne(x => x.Order)
-                    .HasForeignKey(x => x.OrderId);
-            });
-
-            modelBuilder.Entity<OrderItem>(entity =>
-            {
-                entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
-
-                entity.HasOne(d => d.Product)
-                    .WithMany(p => p.OrderItems)
-                    .HasForeignKey(d => d.ProductId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_OrderItems_Products");
-
-                entity.HasOne(d => d.Order)
-                    .WithMany(p => p.OrderItems)
-                    .HasForeignKey(d => d.OrderId)
-                    .OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<Product>(entity =>
-            {
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
-            });
-
-            //OnModelCreatingPartial(modelBuilder);
+            modelBuilder.ApplyConfiguration(Customers.Infrastructure.CustomerMap.GetInstance());
         }
-
-        // partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 
     }
 }
