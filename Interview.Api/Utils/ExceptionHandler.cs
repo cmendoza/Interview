0c9cd9f994728cf8b258f57cdc70f83e0ca1fbda
Carlos Mendoza <cmendozag@outlook.com>
2020-11-22 10:08:04 -0600

    Add ExceptionHandler

---
+++ Interview.Api/Utils/ExceptionHandler.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Interview.Api.Utils
+{
+    public class ExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandler> _logger;
+
+        public ExceptionHandler(RequestDelegate next, ILogger<ExceptionHandler> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception.");
+
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var deeperMessage = GetDeeperMessageInException(exception);
+            var result = JsonConvert.SerializeObject(Envelope.Error(deeperMessage), new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return context.Response.WriteAsync(result);
+        }
+
+        private string GetDeeperMessageInException(Exception ex)
+        {
+            var lastMessage = ex.Message;
+            var innerException = ex.InnerException;
+
+            while (innerException != null)
+            {
+                lastMessage += "-->" + innerException.Message;
+                innerException = innerException.InnerException;
+            }
+
+            return lastMessage;
+        }
+    }
+}
