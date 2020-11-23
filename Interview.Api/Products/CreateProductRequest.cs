bdca123b9747617ecc7a8c4d606a29e8bb22818c
Carlos Mendoza <cmendozag@outlook.com>
2020-11-22 23:13:14 -0600

    Add ProductController and requests

---
+++ Interview.Api/Products/CreateProductRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Interview.Api.Products
+{
+    public class CreateProductRequest
+    {
+        public string  Name  { get;  set; }
+        public decimal Price { get;  set; }
+    }
+}
---
+++ Interview.Api/Products/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Interview.Api.Utils;
+using Interview.BusinessLogic.Products.Application;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Interview.Api.Products
+{
+    public class ProductsController : ApiControllerBase
+    {
+        public ProductsController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateProductRequest request)
+        {
+            var command = new CreateProductCommand(request.Name, request.Price);
+
+            var result = await DispatchAsync(command);
+
+            return FromResult(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, [FromBody] UpdateProductRequest request)
+        {
+            var command = new UpdateProductCommand(id, request.Name, request.Price);
+
+            var result = await DispatchAsync(command);
+
+            return FromResult(result);
+        }
+    }
+}
