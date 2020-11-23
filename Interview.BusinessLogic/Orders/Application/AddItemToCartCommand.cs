a550951a563fe80a71501b95a133c3cb8f753228
Carlos Mendoza <cmendozag@outlook.com>
2020-11-23 13:24:35 -0600

    Add commands to create an order and to add item to order

---
+++ Interview.BusinessLogic/Orders/Application/AddItemToCartCommand.cs
@@ -0,0 +1,54 @@
+﻿using CSharpFunctionalExtensions;
+using Interview.BusinessLogic.Common;
+using Interview.BusinessLogic.Orders.Infrastructure;
+using Interview.BusinessLogic.Products.Infrastructure;
+
+namespace Interview.BusinessLogic.Orders.Application
+{
+    public sealed class AddItemToCartCommand : MediatR.IRequest<Result>
+    {
+        public AddItemToCartCommand(long orderId, long productId, int quantity)
+        {
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+        }
+
+        public long OrderId   { get; }
+        public long ProductId { get; }
+        public int  Quantity  { get; }
+    }
+
+    internal sealed class AddItemToCartCommandHandler : MediatR.RequestHandler<AddItemToCartCommand, Result>
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly OrderRepository _orderRepository;
+        private readonly ProductRepository _productRepository;
+
+        public AddItemToCartCommandHandler(UnitOfWork unitOfWork,
+                                           OrderRepository orderRepository,
+                                           ProductRepository productRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _orderRepository = orderRepository;
+            _productRepository = productRepository;
+        }
+
+        protected override Result Handle(AddItemToCartCommand request)
+        {
+            if (request.Quantity <= 0) return Result.Failure("Quantity must be greater than zero.");
+
+            var order = _orderRepository.Get(request.OrderId);
+            if (order is null) return Result.Failure($"OrderId doesn't exist: {request.OrderId}");
+
+            var product = _productRepository.Get(request.ProductId);
+            if (product is null) return Result.Failure($"ProductId doesn't exist: {request.ProductId}");
+
+            order.AddOrUpdateItem(product, request.Quantity);
+
+            _unitOfWork.Commit();
+
+            return Result.Success();
+        }
+    }
+}
---
+++ Interview.BusinessLogic/Orders/Application/CreateOrderCommand.cs
@@ -0,0 +1,45 @@
+﻿using CSharpFunctionalExtensions;
+using Interview.BusinessLogic.Common;
+using Interview.BusinessLogic.Customers.Infrastructure;
+using Interview.BusinessLogic.Orders.Domain;
+using Interview.BusinessLogic.Orders.Infrastructure;
+
+namespace Interview.BusinessLogic.Orders.Application
+{
+    public sealed class CreateOrderCommand : MediatR.IRequest<Result<long>>
+    {
+        public CreateOrderCommand(long customerId) => CustomerId = customerId;
+
+        public long CustomerId { get; }
+    }
+
+    internal sealed class CreateOrderCommandHandler : MediatR.RequestHandler<CreateOrderCommand, Result<long>>
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly CustomerRepository _customerRepository;
+        private readonly OrderRepository _orderRepository;
+
+        public CreateOrderCommandHandler(UnitOfWork unitOfWork,
+                                         CustomerRepository customerRepository,
+                                         OrderRepository orderRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _customerRepository = customerRepository;
+            _orderRepository = orderRepository;
+        }
+
+        protected override Result<long> Handle(CreateOrderCommand request)
+        {
+            var customer = _customerRepository.Get(request.CustomerId);
+            if (customer is null) return Result.Failure<long>($"CustomerId doesn't exist: {request.CustomerId}");
+
+            var order = new Order(customer);
+
+            _orderRepository.Add(order);
+
+            _unitOfWork.Commit();
+
+            return Result.Success(order.Id);
+        }
+    }
+}
