c888c24198e9110f4b5a1a5a7ea4f393f19291ef
Carlos Mendoza <cmendozag@outlook.com>
2020-11-22 21:46:08 -0600

    Add QueriesConnectionString

---
+++ Interview.Queries/Utils/QueriesConnectionString.cs
@@ -0,0 +1,9 @@
+﻿namespace Interview.Queries.Utils
+{
+    public sealed class QueriesConnectionString
+    {
+        public QueriesConnectionString(string value) => Value = value;
+
+        public string Value { get; }
+    }
+}
